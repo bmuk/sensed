6108335d87df8084cc9f3524c441605384c1495c
Britt Mathis <britt.mathis@gmail.com>
2015-08-04 16:06:09 -0400

    Easy installer for pi

--- install.sh
+++ install.sh
@@ -1,8 +1,46 @@
 #!/bin/bash
-apt-get install curl
+
+# Make sure utilities are installed
+sudo apt-get install -y curl git
+
+# Get nodejs
 curl --silent --location http://deb.nodesource.com/setup_0.12 | sudo bash -
-apt-get install nodejs
+sudo apt-get install -y nodejs
+
+# Create a user which will run sensed
+sudo useradd -G i2c sensed
+sudo mkdir /home/sensed
+sudo chown sensed /home/sensed
+sudo chgrp sensed /home/sensed
+
+# Clone this repo and install dependencies
+su sensed <<EOF
+pushd /home/sensed
+git clone https://github.com/bmuk/sensed
+cd sensed
 cp ./config/config.sample.json config.json
 npm install
-npm install forever -g
-echo 'Update config/config.json to reflect this installation.'
+EOF
+
+# Create systemd service
+sudo cat <<EOF > /etc/systemd/system/sensed.service
+[Service]
+ExecStart=/usr/local/bin/node /home/sensed/sensed/sensed.js
+Restart=always
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=sensed
+User=sensed
+Group=sensed
+Environment=NODE_ENV=development
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable and start sensed
+sudo systemctl enable sensed
+sudo systemctl start sensed
+
+# Let user know the installation is broken (not configured yet)
+echo 'Update /home/sensed/sensed/config/config.json to reflect this installation.'
